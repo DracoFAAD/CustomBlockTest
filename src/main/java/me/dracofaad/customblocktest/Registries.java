a5274585f22b658490a667f9ed5b23d0dcf383a7
DracoFAAD <draco03draco@gmail.com>
2023-06-03 11:07:02 +0200

    Changes

--- src/main/java/me/dracofaad/customblocktest/Registries.java
+++
@@ -1,9 +0,0 @@
-package me.dracofaad.customblocktest;
-
-import net.minecraft.world.level.block.Block;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-
-public class Registries {
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, CustomBlockTest.MODID);
-}
